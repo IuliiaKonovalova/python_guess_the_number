277b04115d9abdafa0311845204d4f1f49342740
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-21 18:49:33 +0800

    Add function which generates a random number and checks whether the guess was correct

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 import random
+
+def guess(x_number):
+  random_number = random.randint(1, x_number)
+  guess = 0
+
+
+  while guess != random_number:
+    guess = int(input(f'Guess a number between 1 and {x_number}: '))
+    if guess < random_number:
+      print("Guess again. Too low")
+    elif guess > random_number:
+      print('Guess again. Too high')
+  
+  print(f'You got it. Random number was {random_number}')
+
+guess(100)
