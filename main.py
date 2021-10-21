6a03fc98d2b6fce3f4f2849bebf93ceadcd51886
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-21 20:03:09 +0800

    Add comments to the functions

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import random
 
 def guess(x_number):
+  """
+  Chooses the random number,
+  checks whether the guess is correct
+  Provides comments to the user which guide them through the choices
+  """
   random_number = random.randint(1, x_number)
   guess = 0
-
 
   while guess != random_number:
     guess = int(input(f'Guess a number between 1 and {x_number}: '))
@@ -27,9 +31,13 @@ def guess(x_number):
 guess(100)
 
 def computer_guess(x_for_computer):
+  """
+  Provides guesses according to the user guidence
+  """
   low = 1
   high = x_for_computer
   feedback = ''
+
   while feedback != 'c':
     if low != high:
       guess = random.randint(low, high)
